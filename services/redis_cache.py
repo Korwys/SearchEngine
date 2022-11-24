694781f4ddc4638c74e8cb4f25b3b8051f2673cb
Koryws <sidorenkodk@gmail.com>
2022-11-24 13:48:16 +0300

    add: logging

--- services/redis_cache.py
+++ services/redis_cache.py
@@ -1,8 +1,11 @@
 import json
+import logging
 
 from redis.exceptions import ConnectionError
 
 from config.setup_manager import manager
+
+logger = logging.getLogger('app.services.redis_cache')
 
 
 def redis_cache(data: list, query: str) -> list:
@@ -11,5 +14,5 @@ def redis_cache(data: list, query: str) -> list:
         redis_data = json.dumps(response, default=str)
         manager.redis.set(query, redis_data, ex=120)
         return response
-    except ConnectionError as e:
-        print(e)
+    except ConnectionError as err:
+        logger.error(err)
